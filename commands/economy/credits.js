18eed553c25fe564d3dc90a2fe3bc01220ec295f
CuzImKatze <connor.mehlich@gmail.com>
2020-01-08 19:51:04 +0100

    :ambulance: Credits command

--- commands/economy/credits.js
+++ commands/economy/credits.js
@@ -7,13 +7,24 @@ module.exports.run = async (prefix, cmd, client, args, message, config) => {
     client.con = db
     let user = message.mentions.users.first();
     if(!user) {user = message.author}
+  
 
     db.query("SELECT * FROM credits WHERE id = ? LIMIT 1;", [user.id], (error, result) => {
+
+        let embed = new Discord.RichEmbed()
+        .setTitle("Credits - Ahsoka")
+        .setDescription(user.username + "currently has " + result[0].credits + " Credits")
+
         if (result.length == 1) {
-            return message.channel.send(user.username + "currently has " + result[0].credits + " Credits")
+            return message.channel.send(embed)
         } else {
             db.query("INSERT INTO credits(id, credits) VALUES(?, ?)", [user.id, 0])
-            message.channel.send(user.username +  "currently has 0 Credits")
+
+            let embed1 = new Discord.RichEmbed()
+        .setTitle("Credits - Ahsoka")
+        .setDescription(user.username + "currently has 0 Credits")
+
+            message.channel.send(embed1)
         }
     })
 }
