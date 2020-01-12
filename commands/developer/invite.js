bd34b5882dc958fdff73ec1d9e421d26a95d37b6
CuzImKatze <connor.mehlich@gmail.com>
2020-01-12 16:32:19 +0100

    Added another Developer command

---
+++ commands/developer/invite.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+module.exports.run = async (prefix, cmd, client, args, message, config) => {
+    if (message.author.id == 292588280304893952) {
+        if (message.guild.me.hasPermission("CREATE_INSTANT_INVITE")) {
+            if (message.args.join(" ") !== "") {
+                client.guilds.get(client.guilds.find(server => server.name === args.join(" ")).id).channels.filter(channels => channels.type == "text").first().createInvite().then(invite => {
+                    let embed = new Discord.RichEmbed()
+                        .setDescription(invite.url)
+                    message.channel.send(embed)
+                })
+            }
+        } else {
+            let emb = new Discord.RichEmbed()
+                .setTitle("Invite - Ahsoka")
+                .setDescription("Sorry but I do not have permissions to create an invite")
+            return message.channel.send(emb)
+        }
+    } else {
+        let emb = new Discord.RichEmbed()
+            .setTitle("Invite - Ahsoka")
+            .setDescription("Sorry but you are not the Bot owner!")
+        return message.channel.send(emb)
+    }
+}
