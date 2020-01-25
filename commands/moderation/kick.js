80e3ffd9fe72a4abead3fefa3965f522ce3bad4b
CuzImKatze <connor.mehlich@gmail.com>
2020-01-25 10:06:26 +0100

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -5,7 +5,7 @@ module.exports.run = async (prefix, cmd, client, args, message, config) => {
     let target = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let reason = args.slice(1).join(' ');
 
-    if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('you do not have permissions to use this command!s');
+    if (!message.member.hasPermission('KICK_MEMBERS')) return message.reply('you do not have permissions to use this command!s');
 
     if (!target) return message.reply('please specify a member to kick!');
     if (!reason) return message.reply('please specify a reason for this kick!');
