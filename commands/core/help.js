87583765767f092dc5d67b1b1e7a7a8a1c8a15b6
CuzImKatze <connor.mehlich@gmail.com>
2020-01-25 22:00:33 +0100

    Updated help command and Credits command

--- commands/core/help.js
+++ commands/core/help.js
@@ -5,13 +5,14 @@ module.exports.run = async (prefix, cmd, client, args, message, config) => {
     let embed = new Discord.RichEmbed()
     .setTitle("Ahsoka - Help")
     .setColor("#67b54c")
-    .addField("⚙Core", "a+help, a+botinfo")
-    .addField("🖼️Images", "a+cat, a+dog, a+fox, a+ahsoka")
-    .addField("🔨Utilities", "a+guildinfo, a+userinfo")
-    .addField("⚖Moderation", "a+ban, a+kick")
-    .addField("💵Economy", "a+credits, a+daily, a+weekly, a+slot")
+    .addField("⚙Core", "`a+help`, `a+botinfo`")
+    .addField("🖼️Images", "`a+cat`, `a+dog`, `a+fox`")
+    .addField("🔨Utilities", "`a+guildinfo`, `a+userinfo`")
+    .addField("⚖Moderation", "`a+ban`, `a+kick`")
+    .addField("💵Economy", "`a+credits`, `a+daily`, `a+weekly`, `a+slot`")
+    .addField("Configuration", "`a+settings`, `a+setmodlog`, `a+setwelcomechannel`, `a+setwelcomemessage`, `a+setleavemessage`")
     if(message.author.id == 292588280304893952) {
-    embed.addField("💻Developer", "a+serverlist, a+update, a+eval")
+    embed.addField("💻Developer", "`a+serverlist`, `a+update`, `a+eval`")
     }    //.addField("🎉Fun", " ")
 
 return message.channel.send(embed);
--- commands/economy/credits.js
+++ commands/economy/credits.js
@@ -6,33 +6,34 @@ module.exports.run = async (prefix, cmd, client, args, message, config) => {
 
     client.con = db
     let user = message.mentions.users.first();
-    if(!user) {user = message.author}
-    if(user.bot == true) {
+    if (!user) {
+        user = message.author
+    }
+    if (user.bot == true) {
         let emb = new Discord.RichEmbed()
-        .setTitle("Credits - Ahsoka")
-        .setColor("#67b54c")
-        .setDescription("Sorry I can't register Bots!")
+            .setTitle("Credits - Ahsoka")
+            .setColor("#67b54c")
+            .setDescription("Sorry I can't register Bots!")
 
         return message.channel.send(emb)
     }
-  
+
 
     db.query("SELECT * FROM credits WHERE id = ? LIMIT 1;", [user.id], (error, result) => {
 
-        let embed = new Discord.RichEmbed()
-        .setTitle("Credits - Ahsoka")
-        .setColor("#67b54c")
-        .setDescription(user.username + " currently has " + result[0].credits + " Credits")
-
         if (result.length == 1) {
+            let embed = new Discord.RichEmbed()
+                .setTitle("Credits - Ahsoka")
+                .setColor("#67b54c")
+                .setDescription(user.username + " currently has " + result[0].credits + " Credits")
             return message.channel.send(embed)
         } else {
             db.query("INSERT INTO credits(id, credits) VALUES(?, ?)", [user.id, 0])
 
             let embed1 = new Discord.RichEmbed()
-        .setTitle("Credits - Ahsoka")
-        .setColor("#67b54c")
-        .setDescription(user.username + " currently has 0 Credits")
+                .setTitle("Credits - Ahsoka")
+                .setColor("#67b54c")
+                .setDescription(user.username + " currently has 0 Credits")
 
             message.channel.send(embed1)
         }
