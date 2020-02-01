d2ed3fa0212fbb87fa62d3f7cd765ba9f11610a0
CuzImKatze <connor.mehlich@gmail.com>
2020-02-01 15:16:07 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
+const active = new Map();
 
 module.exports = async (client, message) => {
   if (message.author.bot) {
     return;
   }
   if (message.channel.type === "dm") {
-    // return message.channel.send("This bot doesn't support DM commands");
+    message.channel.send(`Hey! My name is **Ahsoka**\n I'm a Discord bot created by Katze#1784. I have much commands. You can visit the Following links for Support or invite me:\n **Official Website** https://ahsoka.eu\n **Official Discord** http://discord.ahsoka.eu\n Official Invite link http://invite.ahsoka.eu`)
+    return;
+
   }
 
   let prefix = client.config.prefix;
@@ -14,12 +17,17 @@ module.exports = async (client, message) => {
   let args = messageArray.slice(1);
   let author = message.author;
   let guild = message.guild;
+
+  var ops = {
+    active: active
+  }
+
   if (!message.content.startsWith(prefix)) {
     return;
   }
 
   let commandFile = client.commands.get(cmd.slice(prefix.length));
   if (commandFile) {
-    commandFile.run(prefix, cmd, client, args, message);
+    commandFile.run(prefix, cmd, client, args, message, ops);
  }
 };
