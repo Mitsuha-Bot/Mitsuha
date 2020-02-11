71c04a635580fd45392715484e96fd4649c44b06
CuzImKatze <46849183+CuzImKatze@users.noreply.github.com>
2020-02-11 20:19:58 +0100

    Update setleavemessage.js

--- commands/configuration/setleavemessage.js
+++ commands/configuration/setleavemessage.js
@@ -2,21 +2,21 @@ const Discord = require("discord.js");
 module.exports.run = async (prefix, cmd, client, args, message, config) => {
     let a = args.slice(" ").join(" ")
     let db = client.con;
-    if(!a) return message.reply("Please user a+leavemessage (message). You can user ${user} for the user name and ${server} for the server name!")
+    if(!a) return message.reply(await client.string(message.guild.id, "settings.leaveerror"))
     if(message.member.hasPermission("MANAGE_GUILD")) {
         db.query("SELECT * FROM `settings` WHERE id = ?", [message.guild.id], async (error, result) => {
-            if(result.legnth == 0) {
+            if(result.length == 0) {
                 db.query("INSERT INTO settings(id, modlog, welcomechannel, welcomemessage, leavemessage) VALUES(?, ?, ?, ?, ?)", [message.guild.id, "none", "none", "none", a]);
                 let embed = new Discord.RichEmbed()
                 .setTitle("Configuration - Ladybug")
-                .setDescription(`The leave messag have been set to: ${a}`)
+                .setDescription(await client.string(message.guild.id, "settings.leave") + ` ${a}`)
                 return message.channel.send(embed)
 
             } else {
                 db.query("UPDATE settings SET leavemessage = ? WHERE id = ?", [a, message.guild.id]);
                 let emby = new Discord.RichEmbed()
                 .setTitle("Configuration - Ladybug")
-                .setDescription(`The leave message have been set to: ${a}`)
+                .setDescription(await client.string(message.guild.id, "settings.leave") + ` ${a}`)
                 return message.channel.send(emby)
 
             }
