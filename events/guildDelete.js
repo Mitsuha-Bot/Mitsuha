cdaa25ce4af73da96e4415ccb993509a9093cc17
CuzImKatze <46849183+CuzImKatze@users.noreply.github.com>
2020-02-12 14:44:51 +0100

    Fixing Strings

---
+++ events/guildDelete.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+module.exports = async (client, guild) => {
+    let channel = client.channels.get('677145248048218112')
+    let icon;
+
+    if(guild.iconURL) {
+        icon = guild.iconURL
+    }
+
+    let embed = new Discord.RichEmbed()
+    .setTitle("Ladybug - Left Server")
+    .setThumbnail(icon)
+    .addField("Name", guild.name)
+    .addField("ID", guild.id)
+    .addField("Owner", guild.owner.user.tag)
+    .addField("New Server Counter", client.guilds.size)
+    channel.send(embed)
+}
