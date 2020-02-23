a7363cb6fd0ad13d00cff4a27096ac6d6364843a
CuzImKatze <46849183+CuzImKatze@users.noreply.github.com>
2020-02-23 16:33:38 +0100

    Fixing globalchat perms

--- commands/globalchat/setgermanglobal.js
+++ commands/globalchat/setgermanglobal.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (prefix, cmd, client, args, message, config) => {
     let db = client.con;
     let ch = message.mentions.channels.first()
-    if(!message.member.hasPermission("ADMININSTARTOR")) return message.reply(await client.string(message.guild.id, "settings.noperms"))
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply(await client.string(message.guild.id, "settings.noperms"))
     if(!ch) return message.reply(await client.string(message.guild.id, "settings.ch"))
     db.query("SELECT * FROM `globalde` WHERE guildid = ?", [message.guild.id], async (error , result) => {
         if(result.length == 0) {
