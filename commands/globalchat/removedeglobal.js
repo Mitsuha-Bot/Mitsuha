582f829618e6dd8839d9a2d2b6e5aa6904454ecd
CuzImKatze <46849183+CuzImKatze@users.noreply.github.com>
2020-02-26 20:36:33 +0100

    Fixing MySQL error

--- commands/globalchat/removedeglobal.js
+++ commands/globalchat/removedeglobal.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (prefix, cmd, client, args, message, config) => {
     let db = client.con;
-    db.query("SELETC * FROM globalde WHERE guilid = ?", [message.guild.id], async (error, result) => {
+    db.query("SELETC * FROM globalde WHERE guildid = ?", [message.guild.id], async (error, result) => {
         if(result.length == 0) {
             return message.channel.send("Globalchannel has not been set!")
         } else {
